c39e8870ec19e15959c6dc6158a7609d55efc8f8
Phan Nam <phanam1126@gmail.com>
2020-09-07 09:52:24 +0700

    first commit

---
+++ src/app/html-daotao/danhsach-sv/danhsach-sv.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-danhsach-sv',
+  templateUrl: './danhsach-sv.component.html',
+  styleUrls: ['./danhsach-sv.component.css']
+})
+export class DanhsachSVComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
